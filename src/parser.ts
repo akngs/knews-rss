ab696b5bdea9eb9343e06f31b5b560af97203357
akngs <jania902@gmail.com>
2022-01-10 09:36:03 +0900

    feat: CSV 파싱

---
+++ src/parser.ts
@@ -0,0 +1,59 @@
+import { readCSVObjects } from "https://deno.land/x/csv@v0.7.2/mod.ts";
+
+/** 언론사 */
+export type Publisher = {
+  readonly name: string;
+  readonly type: string;
+  readonly url: string;
+};
+
+/** 피드 명세 */
+export type FeedSpec = {
+  readonly publisher: Publisher;
+  readonly title: string;
+  readonly categories: readonly string[];
+  readonly url: string;
+};
+
+/**
+ * 언론사 목록을 파싱
+ */
+export async function parse_publishers(
+  f: Deno.Reader,
+): Promise<Publisher[]> {
+  const results: Publisher[] = [];
+  for await (const row of readCSVObjects(f)) {
+    results.push({
+      name: row.name || "",
+      type: row.type || "",
+      url: row.url || "",
+    });
+  }
+  return results;
+}
+
+/**
+ * RSS 피드 명세 목록을 파싱
+ */
+export async function parse_feed_specs(
+  publishers: Publisher[],
+  f: Deno.Reader,
+): Promise<FeedSpec[]> {
+  const publisherMap = new Map(publishers.map((p) => [p.name, p]));
+
+  const results: FeedSpec[] = [];
+  for await (const row of readCSVObjects(f)) {
+    const publisher = publisherMap.get(row.publisher);
+    if (publisher === undefined) {
+      throw new Error(`Unknown publisher: ${row.publisher}`);
+    }
+
+    results.push({
+      publisher,
+      title: row.title || "",
+      categories: row.categories ? row.categories.split("|") : [],
+      url: row.url || "",
+    });
+  }
+  return results;
+}
---
+++ src/tests/parser_test.ts
@@ -0,0 +1,53 @@
+import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
+import { StringReader } from "https://deno.land/std@0.120.0/io/mod.ts";
+import {
+  type FeedSpec,
+  parse_feed_specs,
+  parse_publishers,
+  type Publisher,
+} from "../parser.ts";
+
+Deno.test("parse_publishers()", async () => {
+  const raw = new StringReader([
+    "name,type,url",
+    "n0,t0,https://n0.com",
+    "n1,t1,https://n1.com",
+  ].join("\n"));
+
+  const expected: Publisher[] = [
+    { name: "n0", type: "t0", url: "https://n0.com" },
+    { name: "n1", type: "t1", url: "https://n1.com" },
+  ];
+  const actual = await parse_publishers(raw);
+  assertEquals(actual, expected);
+});
+
+Deno.test("parse_feed_specs()", async () => {
+  const raw = new StringReader([
+    "publisher,title,categories,url",
+    "p0,t0,c0,https://p0.com/c0.xml",
+    "p1,t1,c1|c2,https://p1.com/c1.xml",
+  ].join("\n"));
+
+  const publishers = [
+    { name: "p0", type: "t0", url: "https://p0.com" },
+    { name: "p1", type: "t1", url: "https://p1.com" },
+  ];
+
+  const expected: FeedSpec[] = [
+    {
+      publisher: publishers[0],
+      title: "t0",
+      categories: ["c0"],
+      url: "https://p0.com/c0.xml",
+    },
+    {
+      publisher: publishers[1],
+      title: "t1",
+      categories: ["c1", "c2"],
+      url: "https://p1.com/c1.xml",
+    },
+  ];
+  const actual = await parse_feed_specs(publishers, raw);
+  assertEquals(actual, expected);
+});
