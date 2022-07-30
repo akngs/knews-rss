24eb889ffb6d25269617e4854e6667ea2a0dbf1c
akngs <jania902@gmail.com>
2022-07-30 02:37:50 +0000

    refactor: RSS 전체 수집하는 파일을 index에서 collect로 변경

--- src/collect.ts
+++ src/collect.ts
@@ -5,15 +5,15 @@ const feedSpecs = await loadFeedSpecs(
   "./data/feed_specs.csv",
 );
 
-await Deno.mkdir("rss", { recursive: true });
+await Deno.mkdir("rss/raws", { recursive: true });
 
-feedSpecs.forEach(async (spec) => {
+feedSpecs.forEach(async (spec, i) => {
   try {
     // 임시로 랜덤 시간 지연을 넣어서 rate limit 우회
-    await sleep.sleepRandomAmountOfSeconds(1, 20);
+    await sleep.sleep(1 + i % 5);
 
     const categories = spec.categories.join("-");
-    const filename = `rss/${spec.publisher.id}-${categories}.xml`;
+    const filename = `rss/raws/${spec.publisher.id}-${categories}.xml`;
     const feeds = await fetchFeeds(spec);
     const xml = serialize(spec, feeds);
     await Deno.writeTextFile(filename, xml);
